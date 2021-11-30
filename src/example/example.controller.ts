7d537e68034982ed08a17befade242cd2a2b2ce7
wSedlacek <wsedlacekc@gmail.com>
2021-11-29 23:53:19 -0800

    feat: query param

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, MessageEvent, Sse } from '@nestjs/common';
+import { Controller, MessageEvent, Query, Sse } from '@nestjs/common';
 import { interval, mapTo, Observable } from 'rxjs';
 
 @Controller('example')
 export class ExampleController {
   @Sse()
-  public events(): Observable<MessageEvent> {
+  public events(@Query('id') id: string): Observable<MessageEvent> {
+    console.log(id);
     return interval(300).pipe(mapTo({ data: 'Example' }));
   }
 }
